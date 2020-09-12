01f2a1f72151eb5e45664d90e629815280e27b7b
Lama <lamayf@hotmail.com>
2020-09-12 17:23:06 +0300

    Add login function

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //TODO: wrapp with streambulider
+    //TODO: wrapp with StremaProvider
     return MaterialApp(
       home: Wrapper(),
     );

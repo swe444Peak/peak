847489c65b29db6a34f2d7e529052b29f7991046
HaneenAlmohaimeed <60116673+halmohaimeed@users.noreply.github.com>
2020-10-09 01:59:44 +0300

    Add goal incomplete test version

---
+++ lib/screens/test2.dart
@@ -0,0 +1,325 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:peak/models/task.dart';
+import 'package:peak/models/user.dart';
+import 'package:peak/screens/addNewTasks.dart';
+import 'package:peak/screens/shared/commonStyle.dart';
+import 'package:peak/screens/test.dart';
+import 'package:peak/services/databaseServices.dart';
+import 'package:peak/viewmodels/createGoal_model.dart';
+import 'package:provider/provider.dart';
+
+import '../locator.dart';
+import '../services/notification.dart';
+
+class NewGoalPage extends StatefulWidget {
+  @override
+  _NewGoalPageState createState() => _NewGoalPageState();
+}
+
+class _NewGoalPageState extends State<NewGoalPage> {
+  var goalsCounter = 0;
+  NotificationManager notifyManeger = new NotificationManager();
+  String _error;
+  TextEditingController _goalnamecontroller = TextEditingController();
+  DateTime _dateTime = DateTime.now();
+  List<Task> tasks =[];
+  setError(value) => setState(() => _error= value);
+
+  @override
+  void dispose() {
+    _goalnamecontroller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    var user = Provider.of<User>(context);
+    var screenSize = MediaQuery.of(context).size;
+    var width = screenSize.width;
+    var height = screenSize.height;
+
+    return Provider(
+      create: (context)=>tasks,
+          child: StreamProvider<PeakUser>.value(
+          initialData: PeakUser(uid: "", name: ""),
+          value: DatabaseServices().userData(user.uid),
+          builder: (context, snapshot) {
+            return ChangeNotifierProvider<CreateGoalModel>(
+              create: (context) => locator<CreateGoalModel>(),
+              child: Consumer<CreateGoalModel>(
+                builder: (context, model, child) => SafeArea(
+                  child: Scaffold(
+                    backgroundColor: Color.fromRGBO(23, 23, 85, 1.0),
+                    extendBodyBehindAppBar: true,
+                    appBar: AppBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0.0,
+                      bottom: PreferredSize(
+                preferredSize: Size(MediaQuery.of(context).size.width, 50),
+                child: showAlert(),),
+
+                    ),
+                    body: Stack(
+                      children: [
+                        Container(
+                          width: width * 0.8,
+                          height: width * 0.6,
+                          margin: EdgeInsets.fromLTRB(0, 0.0, width * 0.2, 0.0),
+                          decoration: BoxDecoration(
+                              color: Colors.indigo[500],
+                              borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(400)),
+                              gradient: LinearGradient(
+                                  colors: [
+                                    Colors.teal[400],
+                                    Colors.indigo[600],
+                                    Colors.deepPurple[900]
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomCenter)),
+                        ),
+                        
+                        Container(
+                          padding: EdgeInsets.fromLTRB(
+                              width * 0.06, 0.0, width * 0.06, 0.0),
+                          child: ListView(
+                            shrinkWrap: true,
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(
+                                          0.0, 0.0, 0.0, (width * 0.06)),
+                                      child: Text(
+                                        "Add A New Goal",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 34.0,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  height: height*0.88,
+                                //     decoration: BoxDecoration(
+                                // color: Colors.white,
+                                // borderRadius:
+                                //     BorderRadius.all(Radius.circular(20)),
+                                //     ),
+                                    child: Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                      width * 0.05,
+                                      height * 0.03,
+                                      width * 0.05,
+                                      height * 0.03),
+                                child: SingleChildScrollView(
+                                                                  child: ListView(
+                                        scrollDirection: Axis.vertical,
+                                        shrinkWrap: true,
+
+                                        children: [
+                                          
+                                          Card(
+                                            elevation: 20,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+                                                child: Padding(
+                                                  padding: EdgeInsets.all(width * 0.06),
+                                                  child: TextField(
+                                              controller: _goalnamecontroller,
+                                              decoration: InputDecoration(
+                                                  labelText: 'Goal Name',
+                                              ),
+                                            ),
+                                                ),
+                                          ),
+                                          // specialTextField(
+                                          //     _goalnamecontroller,
+                                          //     false,
+                                          //     "Goal name",
+                                          //     model.goalName.error,
+                                          //     model.setGoalName),
+                                          //SizedBox(height: size.height * 0.03),
+                                          // Padding(
+                                          //   padding:
+                                          //       EdgeInsets.fromLTRB(50, 0, 50, 0),
+                                          //   child: Card(
+                                          //     color:
+                                          //         Color.fromRGBO(23, 23, 85, 1.0),
+                                          //     elevation: 0,
+                                          //     shape: RoundedRectangleBorder(
+                                          //       borderRadius:
+                                          //           BorderRadius.circular(20.0),
+                                          //     ),
+                                          //     child: ListTile(
+                                          //       leading: Icon(
+                                          //         Icons.calendar_today,
+                                          //         color: Colors.white,
+                                          //         size: 30,
+                                          //       ),
+                                          //       title: Align(
+                                          //         alignment: Alignment.centerLeft,
+                                          //         child: Text(
+                                          //             _dateTime == null
+                                          //                 ? 'No picked date'
+                                          //                 : DateFormat('dd-MM-yyyy')
+                                          //                     .format(_dateTime),
+                                          //             style: TextStyle(
+                                          //                 fontFamily: 'Montserrat',
+                                          //                 fontSize: 20.0,
+                                          //                 color: Colors.white),
+                                          //             textAlign: TextAlign.left),
+                                          //       ),
+                                          //       onTap: () async {
+                                          //         showDatePicker(
+                                          //                 context: context,
+                                          //                 initialDate:
+                                          //                     _dateTime == null
+                                          //                         ? DateTime.now()
+                                          //                         : _dateTime,
+                                          //                 firstDate: DateTime.now(),
+                                          //                 lastDate: DateTime(2120))
+                                          //             .then((date) {
+                                          //           setState(() {
+                                          //             _dateTime = date;
+                                          //           });
+                                          //         });
+                                          //       },
+                                          //     ),
+                                          //   ),
+                                          // ),
+                                          Card(
+                                            elevation: 20,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+                                              child: Padding(
+                                                padding: EdgeInsets.all(width * 0.06),
+                                                child: TextField(
+                                              decoration: InputDecoration(
+                                                labelText: "Due Date",
+                                                hintText: _dateTime
+                                                    .toString()
+                                                    .split(' ')
+                                                    .first,
+                                                icon: Icon(Icons.calendar_today),
+                                              ),
+                                              readOnly: true,
+                                              onTap: () {
+                                                setState(() {
+                                                  _datePicker(context);
+                                                });
+                                              },
+                                            ),
+                                              ),
+                                          ),
+                                          // SizedBox(height: size.height * 0.04),
+                                          // SizedBox(height: size.height * 0.03),
+                                          // ButtonTheme(
+                                          //   minWidth: 100.0,
+                                          //   height: 50.0,
+                                          //   child: RaisedButton(
+                                          //     splashColor: Colors.teal,
+                                          //     shape: RoundedRectangleBorder(
+                                          //       borderRadius:
+                                          //           BorderRadius.circular(18.0),
+                                          //     ),
+                                          //     color:
+                                          //         Color.fromRGBO(23, 23, 85, 1.0),
+                                          //     onPressed: () {
+                                          //       notifyManeger
+                                          //           .showDeadlineNotification(
+                                          //               'Deadline Reminder',
+                                          //               'The deadline for' +
+                                          //                   _goalnamecontroller
+                                          //                       .text +
+                                          //                   'goal is Tomorrow',
+                                          //               _dateTime);
+                                          //       Navigator.push(
+                                          //           context,
+                                          //           new MaterialPageRoute(
+                                          //               builder: (context) =>
+                                          //                   new NewTaskPage()));
+                                          //     },
+                                          //     textColor: Colors.white,
+                                          //     child: Text('Next',
+                                          //         style: TextStyle(fontSize: 19.0)),
+                                          //   ),
+                                          // ),
+
+                                          AddTask(width,height,setError),
+                                        ],
+                                    ),
+                                ),
+                                      ),
+                                  ),
+                              ],
+                            ),
+                        ),
+                        
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+
+  Future _datePicker(BuildContext context) async {
+    DateTime _picker = await showDatePicker(
+        context: context,
+        initialDate: _dateTime == null ? DateTime.now() : _dateTime,
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2120));
+    if (_picker != null) {
+      setState(() {
+        _dateTime = _picker;
+      });
+    }
+  }
+
+  Widget showAlert() {
+    if (_error != null) {
+      return Container(
+        color: Colors.amberAccent,
+        width: double.infinity,
+        padding: EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.error_outline),
+            ),
+            Expanded(
+              child: Text(_error),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    _error = null;
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    return SizedBox(
+      height: 0,
+    );
+  }
+}
+
+

0293599623d3cb4de17cf2b18b6242fa3597dc40
Lama <lamayf@hotmail.com>
2020-10-08 12:56:39 +0300

    update goalsList and DB services

--- lib/viewmodels/goalsList_model.dart
+++ lib/viewmodels/goalsList_model.dart
@@ -8,7 +8,7 @@ class GoalsListModel extends ChangeNotifier {
   final _firstoreService = locator<DatabaseServices>();
   ViewState _state = ViewState.Idle;
   List<Goal> _goals;
-
+  bool empty = false;
   List<Goal> get goals => _goals;
   ViewState get state => _state;
   void setState(ViewState viewState) {
@@ -20,13 +20,18 @@ class GoalsListModel extends ChangeNotifier {
     setState(ViewState.Busy);
     _firstoreService.getGoals().listen((goalData) {
       List<Goal> updatedGoals = goalData;
-      if (updatedGoals != null && updatedGoals.length > 0) {
-        _goals = updatedGoals;
-        sortGoals();
+      if (updatedGoals != null) {
+        if (updatedGoals.length > 0) {
+          empty = false;
+          _goals = updatedGoals;
+          sortGoals();
+        } else {
+          empty = true;
+        }
         notifyListeners();
       }
       setState(ViewState.Idle);
-    });
+    }, onError: (error) => print(error));
   }
 
   void sortGoals() {

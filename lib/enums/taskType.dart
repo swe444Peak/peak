b6fe4d5a41317ceb81c86cee85c49d313f812ef5
Lama <lamayf@hotmail.com>
2020-10-09 05:28:06 +0300

    udpate task model

--- lib/enums/taskType.dart
+++ lib/enums/taskType.dart
@@ -10,3 +10,12 @@ extension ParseToString on TaskType {
     return this.toString().split('.').last;
   }
 }
+
+extension ParseTaskType on String {
+  TaskType formString() {
+    if (this.toLowerCase() == "once") return TaskType.once;
+    if (this.toLowerCase() == "monthly") return TaskType.monthly;
+    if (this.toLowerCase() == "daily") return TaskType.daily;
+    if (this.toLowerCase() == "weekly") return TaskType.weekly;
+  }
+}
